82cc3b58be351b32e111ee6773f22b98d11b2591
sqreele <kkanthong@gmail.com>
2026-01-20 10:23:05 +0700

    Add dashboard layout wrapper to roster

---
+++ frontend/Lastnext/app/roster/layout.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import DashboardLayout from "../dashboard/layout";
+
+export default function RosterLayout({ children }: { children: React.ReactNode }) {
+  return <DashboardLayout>{children}</DashboardLayout>;
+}
